8d6491e2e99802f1d3b0ae86045bf440e4c390c8
mizutani <shanbenz570@gmail.com>
2025-08-02 18:05:37 +0900

    中.V, homeの改善

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @lineups = current_user.lineups.order(created_at: :desc).limit(3)
+    @players = current_user.players.order(created_at: :desc).limit(4)
   end
 end
