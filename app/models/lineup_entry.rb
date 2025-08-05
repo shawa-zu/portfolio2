30e8b7e12dd05b7bef5f38e7ce0ec486fb44a72c
mizutani <shanbenz570@gmail.com>
2025-08-05 16:15:18 +0900

    プレイヤー、ラインナップのUXの改善

--- app/models/lineup_entry.rb
+++ app/models/lineup_entry.rb
@@ -2,7 +2,7 @@ class LineupEntry < ApplicationRecord
   belongs_to :lineup
   belongs_to :player
   # batting_orderは1~9の整数で、選手の打順をあらわし、重複する数字は許されない
-  validates :player_id, uniqueness: { scope: :lineup_id, message: "同じ選手を複数回登録することはできません" }
+  validates :player_id, presence: true, uniqueness: { scope: :lineup_id, message: "同じ選手を複数回登録することはできません" }
   validates :batting_order, presence: true, uniqueness: { scope: :lineup_id, message: "同じ打順を複数回登録することはできません" },
             numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 9 }
 end
