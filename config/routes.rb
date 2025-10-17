4c0828219efdba2ceb246c15904244c0098ee3b0
mizutani <shanbenz570@gmail.com>
2025-10-17 17:45:39 +0900

    v:前.route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :lineups, only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
-  resources :players, only: [ :index, :new, :create, :edit, :update, :destroy ]
+  resources :lineups, only: [ :index, :show, :new, :create, :edit, :update, :destroy ] do
+    get :auto_complete, on: :collection
+  end
+
+  resources :players, only: [ :index, :new, :create, :edit, :update, :destroy ] do
+    get :auto_complete, on: :collection
+  end
+  
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
