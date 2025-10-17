2436d151e98acabd06e54439dfea9565b0f55c23
mizutani <shanbenz570@gmail.com>
2025-10-17 14:56:27 +0900

    微修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  
+
   Rails.application.routes.default_url_options = {
     host: "www.expected9.com",
     protocol: "https"
